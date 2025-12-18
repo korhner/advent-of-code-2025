b86c14db993db6adfae6aeee74f0a72e5543e78d
ikorhner <ikorhner@godaddy.com>
2025-12-18 12:35:26 +0100

    day5 part 1

---
+++ day5/intervals.go
@@ -0,0 +1,64 @@
+package main
+
+import "sort"
+import "strconv"
+
+type Interval struct {
+	Start int
+	End   int
+}
+
+func (iv Interval) Overlaps(other Interval) bool {
+	return (other.Start >= iv.Start && other.Start <= iv.End) || (iv.Start >= other.Start && iv.Start <= other.End)
+}
+
+func (iv Interval) Contains(point int) bool {
+	return point >= iv.Start && point <= iv.End
+}
+
+type IntervalSet []Interval
+
+func NewIntervalSet(intervals ...Interval) IntervalSet {
+	set := IntervalSet(intervals)
+	sort.Slice(set, func(i, j int) bool {
+		return set[i].Start < set[j].Start
+	})
+
+	merged := make(IntervalSet, 0, len(set))
+	for _, iv := range set {
+		if len(merged) == 0 {
+			merged = append(merged, iv)
+			continue
+		}
+
+		last := &merged[len(merged)-1]
+		if last.Overlaps(iv) {
+			last.Start = min(last.Start, iv.Start)
+			last.End = max(last.End, iv.End)
+		} else {
+			merged = append(merged, iv)
+		}
+	}
+	return merged
+}
+
+func (set IntervalSet) Contains(point int) bool {
+	for _, iv := range set {
+		if point < iv.Start {
+			break
+		}
+
+		if iv.Contains(point) {
+			return true
+		}
+	}
+	return false
+}
+
+func (set IntervalSet) toString() string {
+	result := ""
+	for _, iv := range set {
+		result += "[" + strconv.Itoa(iv.Start) + "-" + strconv.Itoa(iv.End) + "] "
+	}
+	return result
+}
---
+++ day5/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parse() ([]Interval, []int) {
+	scanner := bufio.NewScanner(os.Stdin)
+	parsingIntervals := true
+	var intervals []Interval
+	var points []int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			parsingIntervals = false
+			continue
+		}
+
+		if parsingIntervals {
+			// split line by -
+			parts := strings.Split(line, "-")
+			start, _ := strconv.Atoi(parts[0])
+			end, _ := strconv.Atoi(parts[1])
+
+			intervals = append(intervals, Interval{
+				Start: start,
+				End:   end,
+			})
+		} else {
+			point, _ := strconv.Atoi(line)
+			points = append(points, point)
+		}
+
+	}
+
+	return intervals, points
+}
+func main() {
+	intervals, points := parse()
+
+	fresh := 0
+	intervalsSet := NewIntervalSet(intervals...)
+	fmt.Println("Merged intervals:", intervalsSet.toString())
+	for _, point := range points {
+		if intervalsSet.Contains(point) {
+			fresh++
+			fmt.Println("Point", point, "is fresh")
+		}
+	}
+
+	fmt.Println("Total fresh points:", fresh)
+
+}
